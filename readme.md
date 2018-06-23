e24c5d72be1753ee023219221b1693830492ef45
justin <justin.ch.goh@gmail.com>
2018-06-24 04:07:09 +0800

    linted test.js

--- readme.md
+++ readme.md
@@ -12,22 +12,22 @@ const hd = require('@jgch/html-defects');
 const rules = {
     tagsWithoutAttributes: [
 		{
-			"img": "alt",
+			img: 'alt',
 		},
 		{
-			"a": "rel",
+			a: 'rel',
 		},
 	],
 };
 
 const inputOptions = {
-	'inputMethod': 'string',
-	'source': '<html><img src="foo.jpg" /></html>',
+	inputMethod: 'string',
+	source: '<html><img src="foo.jpg" /></html>',
 	
 }
 
 const outputOptions = {
-	'outputMethod': 'console'
+	outputMethod: 'console'
 }
 
 hd.checkDefects(rules, input, outputOptions);
@@ -48,7 +48,7 @@ This counts the number of tags that do not have a specified attribute.
 // Look for <a> tags that do not contain *rel* attribute.
 tagsWithoutAttributes: [
 	{
-		"a": "rel"
+		a: 'rel'
 	}
 ]
 ```
@@ -62,16 +62,16 @@ This shows if specified tags are missing in the HTML header.
 // and missing <meta name="keywords"> tags.
 headerWithoutTags: [
 	{
-		"title": {}
+		title: {}
 	},
 	{
-		"meta": {
-			"name": '"descriptions"',
+		meta: {
+			name: '"descriptions"', // observe the quotation marks!
 		}
 	},
 	{
-		"meta": {
-			"name": '"keywords"',
+		meta: {
+			name: '"keywords"',
 		}
 	}
 ]
@@ -85,14 +85,14 @@ This compares the quantity of specified tags against a specified quantity given 
 // and check if there is more than 1 <h1> tag.
 tagQuantityComparison: [
 	{
-		"comparisonOperator": ">",
-		"elementName": "strong",
-		"quantity": 15,
+		comparisonOperator: '>',
+		elementName: 'strong',
+		quantity: 15,
 	},
 	{
-		"comparisonOperator": ">",
-		"elementName": "h1",
-		"quantity": 1,
+		comparisonOperator: '>',
+		elementName: 'h1',
+		quantity: 1,
 	}
 ]
 ```
@@ -102,8 +102,8 @@ Specify the input source.
 ```javascript
 // To read from a file.
 inputOptions = {
-	'inputMethod': 'file',
-	'source': 'foo.html'
+	inputMethod: 'file',
+	source: 'foo.html'
 }
 ```
 ## Readable Stream
@@ -111,8 +111,8 @@ inputOptions = {
 // To read from a stream.
 const readableStream = fs.createReadStream('test.html');
 inputOptions = {
-	'inputMethod': 'stream',
-	'source': readableStream
+	inputMethod: 'stream',
+	source: readableStream
 }
 ```
 
@@ -123,15 +123,15 @@ Specify whether to output to a file, a node writable stream or console.
 ```javascript
 // To write to the console.
 outputOptions = {
-	'outputMethod': 'console'
+	outputMethod: 'console'
 }
 ```
 ## File
 ```javascript
 // To write to a file, specify the filename in the 'destination' key.
 outputOptions = {
-	'outputMethod': 'file',
-	'destination': 'defectsFile.txt'
+	outputMethod: 'file',
+	destination: 'defectsFile.txt'
 }
 ```
 
@@ -140,8 +140,8 @@ outputOptions = {
 // To write to a node writable stream, specify the stream in the 'destination' key.
 const writableStream = fs.createWriteStream('defectsStream.txt')
 outputOptions = {
-	'outputMethod': 'stream',
-	'destination': writableStream
+	outputMethod: 'stream',
+	destination: writableStream
 }
 ```
 
--- test.js
+++ test.js
@@ -1,96 +1,96 @@
-const hd = require('./html-defects.js');
 const fs = require('fs');
+const hd = require('./html-defects.js');
 
 const rules = {
   tagsWithoutAttributes: [
     {
-      'img': 'alt',
+      img: 'alt',
     },
     {
-      'a': 'rel',
+      a: 'rel',
     },
   ],
 };
 const inputOptions = {
-  'inputMethod': 'string',
-  'source': '<html><img src="foo.jpg" /><img src="bar.jpg" alt="bar" /></html>',
+  inputMethod: 'string',
+  source: '<html><img src="foo.jpg" /><img src="bar.jpg" alt="bar" /></html>',
 };
 const writableStream = fs.createWriteStream('writeStream.txt');
 const outputOptions = {
-  'outputMethod': 'stream',
-  'destination': writableStream,
+  outputMethod: 'stream',
+  destination: writableStream,
 };
 hd.checkDefects(rules, inputOptions, outputOptions);
 
 const inputOptions2 = {
-  'inputMethod': 'file',
-  'source': 'test.html',
+  inputMethod: 'file',
+  source: 'test.html',
 };
 const outputOptions2 = {
-  'outputMethod': 'console',
+  outputMethod: 'console',
 };
 hd.checkDefects(rules, inputOptions2, outputOptions2);
 
 const readableStream = fs.createReadStream('test.html');
 const inputOptions3 = {
-  'inputMethod': 'stream',
-  'source': readableStream,
+  inputMethod: 'stream',
+  source: readableStream,
 };
 const outputOptions3 = {
-  'outputMethod': 'file',
-  'destination': 'writeFile.txt',
+  outputMethod: 'file',
+  destination: 'writeFile.txt',
 };
 hd.checkDefects(rules, inputOptions3, outputOptions3);
 
 
 const challengeRules = {
   headerWithoutTags: [
-    { 
-      "title": {},
+    {
+      title: {},
     },
-    { 
-      "meta": {
-        "name": '"descriptions"',
+    {
+      meta: {
+        name: '"descriptions"',
       },
     },
     {
-      "meta": {
-        "name": '"keywords"',
+      meta: {
+        name: '"keywords"',
       },
     },
     {
-      "meta": {
-        "name": '"robots"',
+      meta: {
+        name: '"robots"',
       },
     },
   ],
   tagsWithoutAttributes: [
     {
-      "img": "alt",
+      img: 'alt',
     },
     {
-      "a": "rel",
+      a: 'rel',
     },
   ],
   tagQuantityComparison: [
     {
-      "comparisonOperator": ">",
-      "elementName": "strong",
-      "quantity": 15,
+      comparisonOperator: '>',
+      elementName: 'strong',
+      quantity: 15,
     },
     {
-      "comparisonOperator": ">",
-      "elementName": "h1",
-      "quantity": 1,
+      comparisonOperator: '>',
+      elementName: 'h1',
+      quantity: 1,
     },
   ],
 };
 const challengeInputOptions = {
-  'inputMethod': 'string',
-  'source': '<html><head><meta name="descriptions"><meta charset="utf-8"></head><span data-node class="c1 c2"><img src="hello.jpg" /><img src="hello.jpg" alt="hello"/></span><span>Hello i\'m a "test"</span></html>',
+  inputMethod: 'string',
+  source: '<html><head><meta name="descriptions"><meta charset="utf-8"></head><span data-node class="c1 c2"><img src="hello.jpg" /><img src="hello.jpg" alt="hello"/></span><span>Hello i\'m a "test"</span></html>',
 };
 const challengeOutputOptions = {
-  'outputMethod': 'console',
+  outputMethod: 'console',
 };
 
 hd.checkDefects(challengeRules, challengeInputOptions, challengeOutputOptions);

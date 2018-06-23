e0d17050ce20629d69b7b8fa50784ad1296e5c11
justin <justin.ch.goh@gmail.com>
2018-06-23 17:00:22 +0800

    publish npm module

---
+++ html-defects.js
@@ -0,0 +1,464 @@
+const assert = require('assert');
+const fs = require('fs');
+
+// assumptions: encoding in utf-8
+
+// function to check for valid characters in
+// HTML tag or attribute names
+// e.g. h1, meta, data-value
+// HTML 5 allows any character except any type of 
+// space character, ", ', <, >, /, =, control characters,
+// and any non Unicode characters
+
+const validAttributeCharCodes = [
+	9,  // Tab
+	32, // Space
+	34, // "
+	39, // '
+	60, // <
+	62, // >
+	47, // /
+	61, // =
+];
+
+const isCharInCharCodeArray = function(c, arr) {
+	charCode = c.charCodeAt(0);
+	return (arr.indexOf(charCode) === -1) ? false : true;
+}
+assert.deepEqual(["<"].map(
+	char => isCharInCharCodeArray(char, validAttributeCharCodes)),
+	[true]
+)
+
+const isValidAttributeChar = function(c) {
+	return !isCharInCharCodeArray(c, validAttributeCharCodes);
+}
+assert.deepEqual(["a", "A", "0", "-", "_", ":", "."].map(
+	char => isValidAttributeChar(char)),
+	[true, true, true, true, true, true, true]
+)
+assert.deepEqual([" ", "	", "<", ">", "/", "=", "\'", "\""].map(
+	char => isValidAttributeChar(char)),
+	[false, false, false, false, false, false, false, false]
+)
+
+const whiteSpaceCharCodes = [
+	9,  // Tab
+	13, // Carriage return
+	32, // Space
+]
+const isWhiteSpaceChar = function(c) {
+	return isCharInCharCodeArray(c, whiteSpaceCharCodes);
+}
+assert.equal(isWhiteSpaceChar("	"), true)
+assert.equal(isWhiteSpaceChar("\r"), true)
+
+const isAprostrophe = function(c) {
+	return isCharInCharCodeArray(c, [34, 39])
+}
+assert.equal(isAprostrophe('"'), true)
+assert.equal(isAprostrophe("'"), true)
+assert.equal(isAprostrophe(" "), false)
+
+
+// terminology:
+// HTMLCode, HTMLTag, TagName, TagAttribute, TagAttributeValue
+// HTMLTag is the <p> itself, HTMLElement is the stuff between the tags
+ 
+const stripAngleBracketsFromHTMLTag = function(HTMLTag) {
+	const length = HTMLTag.length
+	return (HTMLTag[0] == '<' && HTMLTag[length - 1] == '>') ?
+		HTMLTag.slice(1, length - 1) : HTMLTag
+}
+assert.equal(stripAngleBracketsFromHTMLTag("<html>"), "html")
+assert.equal(stripAngleBracketsFromHTMLTag('<img src="abc.jpg" alt="abc" />'), 'img src="abc.jpg" alt="abc" /')
+assert.equal(stripAngleBracketsFromHTMLTag("Paragraph"), "Paragraph")
+
+const tokenizeStrippedHTMLTag = function(strippedHTMLTag) {
+	// tokenize by parsing what's in between spaces
+	// complication arises due to legitimate spaces within strings
+	// e.g filenames src="smiley face.jpg"
+	const tokens = [];
+	let stringStack = [];
+	let buffer = "";
+
+	const flushBuffer = function() {
+		if (buffer.length !== 0) {
+			tokens.push(buffer);
+		}
+		buffer = "";
+	}
+	
+	const isAprostropheMatchingStackTop = function(char) {
+		return (char === stringStack[stringStack.length - 1]) ? true : false;
+	}
+
+	for (let char of strippedHTMLTag) {
+		char = char.toLowerCase();
+		if (isAprostrophe(char)) {
+			if (isAprostropheMatchingStackTop(char)) {
+				stringStack.pop();
+			} else {
+				if (stringStack.length === 0) {
+					stringStack.push(char);
+				}
+			}
+		}
+
+		// if currently parsing a string
+		if (stringStack.length != 0) {
+			buffer += char;
+		} else {
+
+			if (isWhiteSpaceChar(char)) {
+				flushBuffer();
+			} else {
+				if (char === "=" || char === "/") {
+					flushBuffer();
+					buffer += char;
+					flushBuffer();
+				} else {
+					buffer += char;
+				}
+			} 
+
+		}
+	}
+	flushBuffer();
+	return tokens;
+}
+
+assert.deepEqual(tokenizeStrippedHTMLTag("html"), ["html"]);
+assert.deepEqual(tokenizeStrippedHTMLTag("/html"), ["/", "html"]);
+assert.deepEqual(tokenizeStrippedHTMLTag('meta name = "descriptions" /'), ["meta", "name", "=", '"descriptions"', "/"]);
+// '=' is stripped out as a token even if there is no whitespace surrounding it
+assert.deepEqual(tokenizeStrippedHTMLTag('Meta name="descriptions" /'), ["meta", 'name', '=', '"descriptions"', "/"]);
+assert.deepEqual(tokenizeStrippedHTMLTag('img src="/test/hello 123.jpg" alt="hello" /'), ["img", 'src', '=', '"/test/hello 123.jpg"', 'alt', '=', '"hello"', '/']);
+assert.deepEqual(tokenizeStrippedHTMLTag('img src="mr. foo\'s bar.jpg" alt="mrfoosbar" /'), ['img', 'src', '=', '"mr. foo\'s bar.jpg"', 'alt', '=', '"mrfoosbar"', '/'])
+
+const isAngleBracket = function(c) {
+	return isCharInCharCodeArray(c, [60, 62])
+}
+assert.equal(isAngleBracket("<"), true)
+assert.equal(isAngleBracket(">"), true)
+assert.equal(isAngleBracket("="), false)
+
+const extractHTMLTagsFromHTMLStream = function(stream) {
+	HTMLTags = [];
+	isHTMLTag = false;
+	buffer = "";
+
+	const flushBuffer = function() {
+		if (buffer.length !== 0) {
+			HTMLTags.push(buffer);
+		}
+		buffer = "";
+	}
+
+	for (let char of stream) {
+		if (isAngleBracket(char)) {
+			isHTMLTag = (!isHTMLTag) ? true : false;
+		}
+		if (isHTMLTag) {
+			buffer += char;
+		} else {
+			if (char === ">") {
+				buffer += char;
+			}
+			flushBuffer();
+		}
+	}
+
+	return HTMLTags;
+}
+assert.deepEqual(extractHTMLTagsFromHTMLStream("<html></html>"), ["<html>", "</html>"])
+assert.deepEqual(extractHTMLTagsFromHTMLStream('<html><head><meta charset="utf-8"></head></html>'), ["<html>", "<head>", '<meta charset="utf-8">', "</head>", "</html>"])
+
+const parseTokenizedHTMLTag = function(tokenizedHTMLTag) {
+	let parsedHTMLObj = {};
+	let closingTagTokenPosition = tokenizedHTMLTag.indexOf('/');
+
+	if (closingTagTokenPosition !== -1) {
+		if (closingTagTokenPosition === 0) {
+			parsedHTMLObj["closingTag"] = true;
+		} else if (closingTagTokenPosition === tokenizedHTMLTag.length - 1) {
+			parsedHTMLObj["selfClosingTag"] = true;
+		}
+
+		tokenizedHTMLTag.splice(closingTagTokenPosition, 1)
+	}
+
+	parsedHTMLObj['elementName'] = tokenizedHTMLTag[0];
+	tokenizedHTMLTag.splice(0, 1)
+
+	// attributes with values e.g. <img src="abc.jpg">
+	parsedHTMLObj['attributes'] = {};
+	while (tokenizedHTMLTag.indexOf('=') != -1) {
+		if (!('attributes' in parsedHTMLObj)) {
+			parsedHTMLObj['attributes'] = {}
+		}
+		let equalsSignIndex = tokenizedHTMLTag.indexOf('=');
+		let attributeName = tokenizedHTMLTag[equalsSignIndex - 1];
+		let attributeValue = tokenizedHTMLTag[equalsSignIndex + 1];
+		parsedHTMLObj['attributes'][attributeName] = attributeValue;
+		tokenizedHTMLTag.splice(equalsSignIndex - 1, 3);
+	}
+	// attributes without values e.g. <input disabled>
+	if (tokenizedHTMLTag.length !== 0) {
+		tokenizedHTMLTag.forEach(tag => parsedHTMLObj['attributes'][tag] = null)
+		// parsedHTMLObj['attributesWithoutValues'] = tokenizedHTMLTag;
+	}
+	
+	return parsedHTMLObj;
+}
+assert.deepEqual(parseTokenizedHTMLTag(
+	["img", 'src', '=', '"/test/hello 123.jpg"', 'alt', '=', '"hello"', '/']
+), {
+	"elementName": "img",
+	"attributes": {
+		"src": '"/test/hello 123.jpg"',
+		"alt": '"hello"'
+	},
+	"selfClosingTag": true,
+})
+assert.deepEqual(parseTokenizedHTMLTag(
+	["div", "data-modal-target", "class", "=", "dataset"]
+), {
+	"elementName": "div",
+	"attributes": {
+		"class": 'dataset',
+		"data-modal-target": null,
+	},
+	// "attributesWithoutValues": ["data-modal-target"]
+})
+
+const convertStreamToParsedHTMLTags = function(stream) {
+	parsedHTMLTags = extractHTMLTagsFromHTMLStream(stream)
+		.map((tag) => stripAngleBracketsFromHTMLTag(tag))
+		.map((tag) => tokenizeStrippedHTMLTag(tag))
+		.map((tag) => parseTokenizedHTMLTag(tag));
+	
+	return parsedHTMLTags;
+}
+assert.deepEqual(convertStreamToParsedHTMLTags('<html></html>'), [{
+	'elementName': 'html',
+	'attributes': {},
+},{
+	'elementName': 'html',
+	'closingTag': true,
+	'attributes': {},
+}])
+
+// Use the strategy pattern for rules and output
+class RuleChecker {
+
+	setRule(rule) {
+		this.rule = rule;
+	}
+
+	runRule(input, options) {
+		return this.rule.checkRule(input, options);
+	}
+}
+
+class TagsWithoutAttributesRule {
+	
+	checkRule(input, options) {
+		
+		let output = "";
+		
+		output += `Checking "tags without attributes" rule:\n`;
+
+		for (let option of options) {
+			for (let elementName of Object.keys(option)) {
+				let attributeName = option[elementName];
+				let defectCount = input.filter((tag) => tag['elementName'] === elementName)
+					.filter((tag) => !(Object.keys(tag).includes('closingTag')))
+					.filter((tag) => !(Object.keys(tag['attributes']).includes(attributeName)))
+					.length
+
+				if (defectCount !== 0) {
+					output += `<${elementName}> tags without ${attributeName} attribute: ${defectCount}\n`
+				}
+
+			}
+		}
+		return output;
+	}
+
+}
+
+
+class HeaderWithoutTagsRule {
+	
+	checkRule(input, options) {
+		
+		let output = "";
+		const surroundingTag = "head";
+
+		output += `Checking "header without tags" rule:\n`;
+
+		const filterElementsWithinTag = function(input, tagName) {
+			// filter out elements inside head tag
+			// assuming that there is only one head tag
+			const headTagStartIndex = input.findIndex(tag => tag['elementName'] === tagName)
+			const headTagEndIndex = input.findIndex(tag => (tag['elementName'] === tagName) && tag['closingTag'] === true)
+			input = input.slice(headTagStartIndex + 1, headTagEndIndex)
+			return input
+		}
+		input = filterElementsWithinTag(input, surroundingTag)
+
+		for (let option of options) {
+			for (let elementName of Object.keys(option)) {
+				let attributeRequired = Object.keys(option[elementName])[0];
+				let attributeValueRequired = option[elementName][attributeRequired];
+				let attributeRequiredCount = input.filter(tag => 
+					tag['elementName'] === elementName)
+					.filter(tag => {
+						if (tag['attributes'][attributeRequired] !== undefined) {
+							return tag['attributes'][attributeRequired] === attributeValueRequired;
+						} else {
+							return false
+						}
+					}
+					)
+					.length
+
+				if (attributeRequiredCount === 0) {
+					// don't display empty attributes
+					output += `Header tag does not have <${elementName}${attributeRequired ? ` ${attributeRequired}=${attributeValueRequired}` : ''}>\n`
+				}
+			}
+		}
+		return output;
+	}
+}
+
+const compareQuantityMap = {
+	'<': (x, y) => x < y,
+	'>': (x, y) => x > y,
+	'==': (x, y) => x === y,
+	'>=': (x, y) => x >= y,
+	'<=': (x, y) => x <= y,
+	'!=': (x, y) => x != y,
+}
+
+const compareQuantity = function(x, y, comparisonOperator) {
+	return compareQuantityMap[comparisonOperator](x, y)
+}
+assert.equal(compareQuantity(1, 2, '<'), true)
+assert.equal(compareQuantity(5, 2, '>'), true)
+assert.equal(compareQuantity(2, 2, '>='), true)
+assert.equal(compareQuantity(2, 2, '<='), true)
+assert.equal(compareQuantity(2, 2, '=='), true)
+assert.equal(compareQuantity(5, 2, '!='), true)
+
+
+class TagQuantityComparisonRule {
+	
+	checkRule(input, options) {
+
+		let output = "";
+		let surroundingTag = "html";
+
+		output += `Checking "tag quantity comparison" rule:\n`;
+		
+		const filterElementsWithinTag = function(input, tagName) {
+			// filter out elements inside head tag
+			// assuming that there is only one head tag
+			const headTagStartIndex = input.findIndex(tag => tag['elementName'] === tagName)
+			const headTagEndIndex = input.findIndex(tag => (tag['elementName'] === tagName) && tag['closingTag'] === true)
+			input = input.slice(headTagStartIndex + 1, headTagEndIndex)
+			return input
+		}
+		input = filterElementsWithinTag(input, surroundingTag)
+
+		for (let option of options) {
+
+			let tagCount = input.filter(tag => (tag['elementName'] === option['elementName'] && !tag['closingTag']))
+				.length
+
+			if (compareQuantity(tagCount, option['quantity'], option['comparisonOperator'])) {
+				output += `In <${surroundingTag}>: Quantity of <${option['elementName']}> tags is ${option['comparisonOperator']} ${option['quantity']}\n`;
+			}
+			// assert option contains keys comparisonOperator,
+			// elementName, quantity
+			// throw errors
+		}
+
+		return output;
+	}
+}
+
+
+class OutputHandler {
+
+	setOutputMethod(outputMethod) {
+		this.outputMethod = outputMethod;
+	}
+
+	writeOutput(output, outputDestination) {
+		this.outputMethod.writeOutput(output, outputDestination);
+	}
+}
+
+class ConsoleOutputMethod {
+
+	writeOutput(output, outputDestination) {
+		console.log(output)
+	}
+
+}
+
+class FileOutputMethod {
+
+	writeOutput(output, outputDestination) {
+		fs.writeFile(outputDestination, output, (err) => {
+			if (err) throw err;
+			console.log('File saved.')
+		})
+	}
+
+}
+
+class WritableStreamMethod {
+
+	writeOutput(output, outputDestination) {
+		outputDestination.write(output);
+	}
+
+}
+
+// like 'main'
+const checkDefects = function(rules, input, outputOptions) {
+
+	input = input.toString();
+
+	input = convertStreamToParsedHTMLTags(input);
+	
+	output = "";
+	const ruleMap = {
+		'tagsWithoutAttributes': new TagsWithoutAttributesRule(),
+		'headerWithoutTags': new HeaderWithoutTagsRule(),
+		'tagQuantityComparison': new TagQuantityComparisonRule(),
+	}
+
+	const outputMap = {
+		'console': new ConsoleOutputMethod(),
+		'file': new FileOutputMethod(),
+		'stream': new WritableStreamMethod(),
+	}
+
+	const checker = new RuleChecker();
+	for (let rule of Object.keys(rules)) {
+		checker.setRule(ruleMap[rule]);
+		output += checker.runRule(input, rules[rule]);
+		output += "\n";
+	}
+
+	const outputHandler = new OutputHandler();
+	outputHandler.setOutputMethod(outputMap[outputOptions['outputMethod']])
+	outputHandler.writeOutput(output, outputOptions['stream'])
+}
+
+module.exports.checkDefects = checkDefects;
---
+++ readme.md
@@ -0,0 +1,134 @@
+# Installation
+
+```javascript
+npm install --save @jgch/html-defects
+```
+
+# Getting started
+
+```javascript
+const hd = require('@jgch/html-defects');
+
+// See Rules section below
+const rules = {
+	tagsWithoutAttributes: [
+		{
+			"img": "alt",
+		},
+		{
+			"a": "rel",
+		},
+	],
+};
+
+// Can be a string, file, or node readable stream.
+const input = '<html><img src="foo.jpg" /></html>';
+const outputOptions = {
+	'outputMethod': 'console'
+}
+
+hd.checkDefects(rules, input, outputOptions);
+```
+
+# Rules
+The rules object can have any of these keys:
+1. tagsWithoutAttributes
+2. headerWithoutTags
+3. tagQuantityComparison
+
+Each rule key contains an array of options.
+
+## Tags without attributes
+This counts the number of tags that do not have a specified attribute.
+
+```javascript
+// Look for <a> tags that do not contain *rel* attribute
+tagsWithoutAttributes: [
+	{
+		"a": "rel"
+	}
+]
+```
+
+## Header not containing tags
+This shows if specified tags are missing in the HTML header.
+
+```javascript
+// Check for missing <title> tags and 
+// missing <meta name="descriptions"> tags
+headerWithoutTags: [
+	{
+		"title": {}
+	},
+	{
+		"meta": {
+			"name": '"descriptions"',
+		}
+	},
+	{
+		"meta": {
+			"name": '"keywords"',
+		}
+	}
+]
+```
+
+## Tag Quantity Comparison
+This compares the quantity of specified tags against a specified quantity given a specified comparison operator.
+
+```javascript
+// Check if there are more than 15 <strong> tags,
+// and check if there is more than 1 <h1> tag.
+tagQuantityComparison: [
+	{
+		"comparisonOperator": ">",
+		"elementName": "strong",
+		"quantity": 15,
+	},
+	{
+		"comparisonOperator": ">",
+		"elementName": "h1",
+		"quantity": 1,
+	}
+]
+```
+# Input
+Specify the input source.
+## HTML File
+```javascript
+const input = fs.readFileSync('test.html')
+```
+## Readable Stream
+```javascript
+const input = fs.createReadStream('test.html')
+```
+
+
+# Output options
+Specify whether to output to a file, a node writable stream or console.
+## Console
+```javascript
+// To write to the console.
+outputOptions = {
+	'outputMethod': 'console'
+}
+```
+## File
+```javascript
+// To write to a file, specify the filename in the 'stream' key.
+outputOptions = {
+	'outputMethod': 'file',
+	'stream': 'defects.txt'
+}
+```
+
+## Writable Stream
+```javascript
+// To write to a node writable stream, specify the stream in the 'stream' key.
+const writableStream = fs.createWriteStream('defects.txt')
+outputOptions = {
+	'outputMethod': 'file',
+	'stream': writableStream
+}
+```
+
